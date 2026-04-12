326a4b51d437666a94143722d052a3f5c67c9fc9
Terialion <wyh2022@mail.ustc.edu.cn>
2026-04-12 11:36:24 +0800

    First commit.

---
+++ labsite/__init__.py
@@ -0,0 +1,1 @@
+"""Static site generator for the website directory."""
---
+++ preview.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pushd /Users/zevin/Library/CloudStorage/OneDrive-个人/code/agents/website
+.venv\Scripts\python preview.py --port 8000
